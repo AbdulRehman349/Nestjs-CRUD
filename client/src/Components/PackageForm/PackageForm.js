4ef7f8cb7100cbc211ccd79f2515afdd3ea0d119
Abdul Rehman <abdulrehmanssc10@gmail.com>
2022-10-26 11:34:57 +0500

    integrated with frontend

--- client/src/Components/PackageForm/PackageForm.js
+++ client/src/Components/PackageForm/PackageForm.js
@@ -23,11 +23,12 @@ const PackageForm = () => {
     const dispatch = useDispatch()
     const navigate = useNavigate()
     let { id } = useParams()
-    const [packages, setPackages] = useState({ packagename: '', packagedescription: '', servicesArr: [] })
+    const [packages, setPackages] = useState({ packagename: '', description: '', servicesArr: [] })
     const [allservices, setAllServices] = useState([])
     let services = useSelector((state) => state.service)
 
     let singlepackage = useSelector((state) => state.singlepackage)
+
 
     useEffect(() => {
         setAllServices([...services])
@@ -132,7 +133,7 @@ const PackageForm = () => {
                 </div>
                 <div className="mb-6">
                     <label htmlFor="base-input" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Description</label>
-                    <input type="text" id="base-input" value={packages.packagedescription} onChange={(e) => setPackages({ ...packages, packagedescription: e.target.value })} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+                    <input type="text" id="base-input" value={packages.description} onChange={(e) => setPackages({ ...packages, description: e.target.value })} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
                 </div>
                 <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
             </form>
